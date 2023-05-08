b839980f69b2fdd9378ac920b29c1e272a43cf9f
PolicyPuma4 <83885295+PolicyPuma4@users.noreply.github.com>
2023-05-08 09:23:04 +0100

    Add files via upload

--- build.bat
+++ build.bat
@@ -5,3 +5,11 @@ go.exe build -o swtr_windows_amd64.exe -ldflags="-s -w" cmd\surfshark-wireguard-
 set GOOS=linux
 set GOARCH=amd64
 go.exe build -o swtr_linux_amd64 -ldflags="-s -w" cmd\surfshark-wireguard-tunnel-generator\main.go
+
+set GOOS=darwin
+set GOARCH=amd64
+go.exe build -o swtr_darwin_amd64 -ldflags="-s -w" cmd\surfshark-wireguard-tunnel-generator\main.go
+
+set GOOS=darwin
+set GOARCH=arm64
+go.exe build -o swtr_darwin_arm64 -ldflags="-s -w" cmd\surfshark-wireguard-tunnel-generator\main.go
